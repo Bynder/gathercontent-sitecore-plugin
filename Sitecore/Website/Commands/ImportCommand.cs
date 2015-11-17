ee35beec9a397684fdaaf8f60d990646b5e72da8
Vlad Ziziko <v.ziziko@brimit.com>
2015-11-17 14:12:30 +0300

    added Import Command; added knockout-3.4.0.js

---
+++ Sitecore/Website/Commands/ImportCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+
+namespace GatherContent.Connector.Website.Commands
+{
+    public class ImportCommand : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            try
+            {
+                Assert.ArgumentNotNull(context, "context");
+                if (context.Items.Length != 1)
+                {
+                    return;
+                }
+                var item = context.Items[0];
+                var parameters = new NameValueCollection
+                                     {
+                                         { "id", item.ID.ToString() }, 
+                                         { "language", item.Language.ToString() }, 
+                                         { "version", item.Version.ToString() }, 
+                                         { "load", StringUtil.GetString(new[] { context.Parameters["load"] }) }, 
+                                     };
+
+                Context.ClientPage.Start(this, "Run", parameters);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message, ex, typeof(ImportCommand));
+                SheerResponse.Alert("Sync failed. See log for details.");
+            }
+        }
+
+        protected void Run(ClientPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            var id = args.Parameters["id"].Replace("{", "").Replace("}", "");
+            var language = Language.Parse(args.Parameters["language"]);
+            var version = args.Parameters["version"];
+            var uri = new UrlString(UIUtil.GetUri("control:Import"));
+            var path = string.Format("{0}&id={1}&l={2}&v={3}", uri, id, language, version);
+
+            var options = new ModalDialogOptions(path)
+            {
+                Width = "800",
+                Height = "600",
+                MinWidth = "600",
+                MinHeight = "400",
+                Maximizable = false,
+                Header = "Import Content from GatherContent"
+            };
+
+            Context.ClientPage.ClientResponse.Broadcast(Context.ClientPage.ClientResponse.ShowModalDialog(options), "Shell");
+        }
+    }
+}

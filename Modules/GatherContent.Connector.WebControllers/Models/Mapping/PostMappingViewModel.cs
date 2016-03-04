2b738123274d633491df368f54519c2624ab6a05
d.pashuk <d.pashuk@brimit.com>
2016-03-04 18:26:37 +0300

    missing file

---
+++ Modules/GatherContent.Connector.WebControllers/Models/Mapping/PostMappingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GatherContent.Connector.WebControllers.Models.Mapping
+{
+    public class PostMappingViewModel
+    {
+        public PostMappingViewModel()
+        {
+            TemplateTabs = new List<TemplateTab>();
+        }
+
+        public List<TemplateTab> TemplateTabs { get; set; }
+        public bool IsEdit { get; set; }
+        public string TemplateId { get; set; }
+        public string SelectedTemplateId { get; set; }
+        public string GcMappingTitle { get; set; }
+        public string ScMappingId { get; set; }
+        public string DefaultLocation { get; set; }
+    }
+}

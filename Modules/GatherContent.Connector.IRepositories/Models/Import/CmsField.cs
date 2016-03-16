a2cfd440ecb517cda4e29f17383d3033b5b8d0ca
d.pashuk <d.pashuk@brimit.com>
2016-03-16 12:02:35 +0300

    separation IRepositories classes

--- Modules/GatherContent.Connector.IRepositories/Models/Import/CmsField.cs
+++ Modules/GatherContent.Connector.IRepositories/Models/Import/CmsField.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿
 namespace GatherContent.Connector.IRepositories.Models.Import
 {
     public class CmsField
@@ -8,23 +6,5 @@ namespace GatherContent.Connector.IRepositories.Models.Import
         public CmsTemplateField TemplateField { get; set; }
 
         public object Value { get; set; }
-    }
-
-    public class FieldValueOptions
-    {
-        public List<string> Options { get; set; }
-    }
-
-    public class FieldValueFiles
-    {
-        public List<File> Files { get; set; }
-    }
-
-    public class File
-    {
-        public string Url { get; set; }
-        public string FileName { get; set; }
-        //public string FieldId { get; set; }
-        public DateTime UpdatedDate { get; set; }
     }
 }
---
+++ Modules/GatherContent.Connector.IRepositories/Models/Import/File.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GatherContent.Connector.IRepositories.Models.Import
+{
+    public class File
+    {
+        public string Url { get; set; }
+        public string FileName { get; set; }
+        //public string FieldId { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}

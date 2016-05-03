947741527d1a51a3393f71c12ad25ca9b60ed468
danl <d.raschupkin@brimit.com>
2016-05-03 18:53:01 +0300

    EE support experiment

--- Sitecore 8.1/Website/sitecore modules/Shell/GatherContent/Experience Editor/GatherContentMLImport.js
+++ Sitecore 8.1/Website/sitecore modules/Shell/GatherContent/Experience Editor/GatherContentMLImport.js
@@ -1,4 +1,5 @@
-﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
+﻿/* normal version, works in sc8.1 only
+define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
     Sitecore.Commands.GCMLImportCommand =
   {
     canExecute: function (context) {
@@ -17,4 +18,56 @@
       });
     },
   };
+});
+
+*/
+
+/* ugly version, may be works in 8.0-8.1 */
+define(["sitecore"], function (Sitecore) {
+    setTimeout(
+	function () {
+	    var d = Sitecore.ExperienceEditor.Dialogs;
+	    if (typeof d != 'undefined') {
+	        Sitecore.Commands.GCMLImportCommand =
+              {
+                  canExecute: function (context) {
+                      return true;
+                  },
+                  execute: function (context) {
+
+                      var itemId = context.currentContext.itemId.replace("%7b", "").replace("%7d", "");
+
+                      var dialogPath = "/sitecore modules/shell/gathercontent/import/MultiLocationImport.html?id=" + itemId + "&l=" + context.currentContext.language + "&v=" + context.currentContext.version + "&db=" + context.currentContext.database;
+                      var dialogFeatures = "dialogHeight: 700px;dialogWidth: 1200px;";
+                      Sitecore.ExperienceEditor.Dialogs.showModalDialog(dialogPath, '', dialogFeatures, null, function (result) {
+                          if (!result) {
+                              return;
+                          }
+                      });
+                  },
+              };
+	    }
+	    else {
+	        define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
+	            Sitecore.Commands.GCMLImportCommand =
+              {
+                  canExecute: function (context) {
+                      return true;
+                  },
+                  execute: function (context) {
+
+                      var itemId = context.currentContext.itemId.replace("%7b", "").replace("%7d", "");
+
+                      var dialogPath = "/sitecore modules/shell/gathercontent/import/MultiLocationImport.html?id=" + itemId + "&l=" + context.currentContext.language + "&v=" + context.currentContext.version + "&db=" + context.currentContext.database;
+                      var dialogFeatures = "dialogHeight: 700px;dialogWidth: 1200px;";
+                      ExperienceEditor.Dialogs.showModalDialog(dialogPath, '', dialogFeatures, null, function (result) {
+                          if (!result) {
+                              return;
+                          }
+                      });
+                  },
+              };
+	        });
+	    }
+	}, 200);
 });

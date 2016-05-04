3f5bc8ab28197bec50107d374c032ea5e4fbdd88
danl <d.raschupkin@brimit.com>
2016-05-04 12:37:37 +0300

    ee buttons 8.0 - 8.1

--- Sitecore 8.1/Website/sitecore modules/Shell/GatherContent/Experience Editor/8.0/GatherContentImport.js
+++ Sitecore 8.1/Website/sitecore modules/Shell/GatherContent/Experience Editor/8.0/GatherContentImport.js
@@ -1,4 +1,4 @@
-﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function(sitecore, experienceEditor) {
+﻿define(["sitecore"], function(sitecore) {
     sitecore.Commands.GCImportCommand =
     {
         canExecute: function(context) {
@@ -8,7 +8,7 @@
             var itemId = context.currentContext.itemId.replace("%7b", "").replace("%7d", "");
             var dialogPath = "/sitecore modules/shell/gathercontent/import/import.html?id=" + itemId + "&l=" + context.currentContext.language + "&v=" + context.currentContext.version + "&db=" + context.currentContext.database;
             var dialogFeatures = "dialogHeight: 700px;dialogWidth: 1200px;";
-            experienceEditor.Dialogs.showModalDialog(dialogPath, '', dialogFeatures, null, function(result) {
+            sitecore.ExperienceEditor.Dialogs.showModalDialog(dialogPath, '', dialogFeatures, null, function(result) {
                 if (!result) {
                     return;
                 }
